32496d14cce35b25a80e1ba6daafe35ca3fd68db
ARXARC <159053836+ARXARC@users.noreply.github.com>
2024-03-06 21:32:20 -0600

    Update canasta.py

--- canasta.py
+++ canasta.py
@@ -1,28 +1,24 @@
 lista = []
-def agregar(item):
-  lista.append(item)
-def remover(item):
-  if item in lista:
-    lista.remove(item)
-def visualizar():
-  print(lista)
+print("Opciones:\n","1. Añadir item\n","2. Remover item\n","3. Visualizar lista\n","4. Salir\n")
 while True:
-  print("Opciones:")
-  print("1. Añadir item")
-  print("2. Remover item")
-  print("3. Visualizar lista")
-  print("4. Salir")
-  opcion = int(input("Introduzca una opción: "))
-  if opcion == 1:
-    item = input("Introduzca el item a añadir: ")
-    agregar(item)
-  elif opcion == 2:
-    item = input("Introduzca el item a eliminar: ")
-    remover(item)
-  elif opcion == 3:
-    visualizar()
-  elif opcion == 4:
-    print("¡Hasta luego!")
-    break
-  else:
-    print("Opción no válida")
+    opcion = input("Introduzca una opción: ")
+    if opcion == "1":
+        item = input("Introduzca el item a añadir: ")
+        lista.append(item)
+        print(f"Añadido {item} a la lista de compras.")
+    elif opcion == "2":
+        item = input("Introduzca el item a eliminar: ")
+        if item in lista:
+            lista.remove(item)
+            print(f"Eliminado {item} de la lista de compras.")
+        else:
+            print(f"{item} no existe ese item")
+    elif opcion == "3":
+        print("Lista de compras:")
+        for i, item in enumerate(lista, start=1):
+            print(f"{i}. {item}")
+    elif opcion == "4":
+        print("Adios")
+        break
+    else:
+        print("Opción no válida.")
